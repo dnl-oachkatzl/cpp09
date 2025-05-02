206de70eb66e22e08deaba438991329a86113c4b
Daniel Springer <daspring@student.42heilbronn.de>
2025-05-03 00:42:28 +0200

    setup RPN class to OCF

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,8 +6,20 @@
 /*   By: daspring <daspring@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 00:06:07 by daspring          #+#    #+#             */
-/*   Updated: 2025/05/03 00:09:12 by daspring         ###   ########.fr       */
+/*   Updated: 2025/05/03 00:32:16 by daspring         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <sstream>
+#include <iostream>
+
 #include "RPN.hpp"
+
+RPN::RPN() {}
+RPN::~RPN() {}
+
+void	RPN::runOperations(const std::string input) {
+	// std::stringstream ss(input);
+	std::cout << input << "\n";
+}
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,16 +6,26 @@
 /*   By: daspring <daspring@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 00:04:57 by daspring          #+#    #+#             */
-/*   Updated: 2025/05/03 00:09:34 by daspring         ###   ########.fr       */
+/*   Updated: 2025/05/03 00:28:19 by daspring         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RPN_HPP
 #define RPN_HPP
 
+#include <stack>
+
 class	RPN {
 public:
+	RPN();
+	RPN(const RPN& other) = delete;
+	~RPN();
+
+	RPN&	operator=(const RPN& other) = delete;
+
+	void	runOperations(const std::string input);
 private:
+	std::stack<int> numbers_stack_;
 };
 
 #endif	//RPN_HPP
